bcd9b2edfc528177bc4712e791f569e81f6a0f74
Jaehyeon Kim <65964601+Jaehyeon1020@users.noreply.github.com>
2024-06-30 00:12:15 +0900

    sec6_problem3_add_dfs

--- nossi/sec6/3.py
+++ nossi/sec6/3.py
@@ -1,14 +1,11 @@
 from collections import deque
 
-def can_visit_all(rooms):
+def can_visit_all_bfs(rooms):
     q = deque()
     visited = [False for _ in range(len(rooms))]
-    visited[0] = True
 
-    for room in rooms[0]:
-        if visited[room] is False:
-            q.append(room)
-            visited[room] = True
+    q.append(0)
+    visited[0] = True
 
     while q:
         key = q.popleft()
@@ -21,9 +18,25 @@ def can_visit_all(rooms):
         return False
     return True
 
+def can_visit_all_dfs(rooms):
+    visited = [False for _ in range(len(rooms))]
+
+    def dfs(v):
+        visited[v] = True
+        for room in rooms[v]:
+            if visited[room] is False:
+                dfs(room)
+    dfs(0)
+
+    if False in visited:
+        return False
+    return True
+
+
 rooms1 = [[1], [2], [3], []]
 rooms2 = [[1,3], [3,0,1], [2], [0]]
 rooms3 = [[0,1], [0,2], [], [0,1,2,3]]
 
-print(can_visit_all(rooms3))
+print(can_visit_all_bfs(rooms1))
+print(can_visit_all_dfs(rooms1))
 
