93818211f05edf3a098b9e0724c9a0577d5255ad
Jaehyeon Kim <65964601+Jaehyeon1020@users.noreply.github.com>
2024-07-03 14:48:21 +0900

    sec7_problem2_add_bottom_up

--- nossi/sec7/2.py
+++ nossi/sec7/2.py
@@ -11,3 +11,14 @@ def getMinimum(n):
     return memo[n]
 
 print(getMinimum(len(cost)))
+
+memo2 = {0: 0, 1: 0}
+def getMinimum_bottomup(n):
+    if n == 0 or n == 1:
+        return memo2[n]
+    
+    for i in range(2, n + 1):
+        memo2[i] = min(memo2[i - 2] + cost[i - 2], memo2[i - 1] + cost[i - 1])
+    return memo2[n]
+
+print(getMinimum_bottomup(len(cost)))
