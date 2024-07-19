81ff5379ac5bd3dc5732b1801a92b33ef15a75f7
Jaehyeon Kim <kjhkk1020@g.skku.edu>
2024-07-19 20:49:24 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 41.40 ms, Memory: 39 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,15 +1,21 @@
 def solution(participant, completion):    
-    d = {}
+    par_dict = {}
+    comp_dict = {}
     
-    for p in participant:
-        if p in d:
-            d[p] += 1
-        else:
-            d[p] = 0
+    for cur in participant:
+        if cur not in par_dict:
+            par_dict[cur] = 1
+            continue
+        par_dict[cur] += 1
+        
+    for cur in completion:
+        if cur not in comp_dict:
+            comp_dict[cur] = 1
+            continue
+        comp_dict[cur] += 1
     
-    for c in completion:
-        if c not in d:
-            return c
-        d[c] -= 1
-    
-    return max(d, key=d.get)
+    for cur in participant:
+        if cur not in comp_dict:
+            return cur
+        if par_dict[cur] != comp_dict[cur]:
+            return cur
