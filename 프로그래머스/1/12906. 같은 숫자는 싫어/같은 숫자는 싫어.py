7608e86ec051adfc8c492b99d8fedab18a478b87
Jaehyeon Kim <kjhkk1020@g.skku.edu>
2024-07-20 20:44:29 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 62.65 ms, Memory: 27.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -0,0 +1,12 @@
+def solution(arr):
+    stack = []
+    
+    for num in arr:
+        if not stack:
+            stack.append(num)
+            continue
+        if stack[-1] != num:
+            stack.append(num)
+            
+    return stack
+            
