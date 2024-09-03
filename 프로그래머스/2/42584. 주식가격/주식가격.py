d2aa84467273831e3e4ed70aa4609f76a49a96eb
Jaehyeon Kim <kjhkk1020@g.skku.edu>
2024-09-04 00:14:50 +0900

    [level 2] Title: 주식가격, Time: 43.26 ms, Memory: 19.5 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.py
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -1,13 +1,19 @@
 def solution(prices):
     answer = [0 for x in range(len(prices))]
     
-    for i, price in enumerate(prices):
-        for j in range(i + 1, len(prices)):
-            if prices[j] < price:
-                answer[i] = j - i
-                break
-            else:
-                answer[i] += 1
+    stack = [[0, prices[0]]]
+    
+    time = 1
+    while time < len(prices):
+        while stack and prices[time] < stack[-1][1]:
+            cur = stack.pop()
+            answer[cur[0]] = time - cur[0]
+        stack.append([time, prices[time]])
+        time += 1
+    
+    while stack:
+        cur = stack.pop()
+        answer[cur[0]] = (len(prices) - 1) - cur[0]
     
     return answer
     
