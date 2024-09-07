f9c58c6baf3173d4094eb10da00a74c10ae1ac3b
Jaehyeon Kim <kjhkk1020@g.skku.edu>
2024-09-07 10:49:57 +0900

    [level 2] Title: H-Index, Time: 259.95 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.py
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -1,11 +1,14 @@
 def solution(citations):
-    citation_max = max(citations)
+    citations.sort(reverse = True)
+    citation_max = citations[0]
     
     while citation_max >= 0:
         count = 0
         for citation in citations:
             if citation >= citation_max:
                 count += 1
+            if citation < citation_max:
+                break
         
         if count >= citation_max:
             return citation_max
