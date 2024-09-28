b5014aa7edf9db83a8cc552ea87f210de28303f5
Jaehyeon Kim <kjhkk1020@g.skku.edu>
2024-09-28 14:44:15 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 0.79 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.py
@@ -0,0 +1,51 @@
+# 14:19 - 
+
+def solution(str1, str2):
+    str1, str2 = str1.lower(), str2.lower()
+    
+    str1_tokens = {}
+    str2_tokens = {}
+    
+    # str1 토큰
+    for i in range(len(str1) - 1):
+        token = str1[i:i+2]
+        if token.isalpha():
+            if token in str1_tokens:
+                str1_tokens[token] += 1
+            else:
+                str1_tokens[token] = 1
+    
+    # str2 토큰
+    for i in range(len(str2) - 1):
+        token = str2[i:i+2]
+        if token.isalpha():
+            print(token)
+            if token in str2_tokens:
+                str2_tokens[token] += 1
+            else:
+                str2_tokens[token] = 1
+    
+    intersec = []
+    union = []
+    
+    # 교집합, 합집합(str1)
+    for key in str1_tokens:
+        count_1 = str1_tokens[key]
+        
+        if key in str2_tokens:
+            count_2 = str2_tokens[key]
+            intersec.extend([key] * min(count_1, count_2))
+            union.extend([key] * max(count_1, count_2))
+        else:
+            union.extend([key] * count_1)
+            
+    # 합집합
+    for key in str2_tokens:
+        count_2 = str2_tokens[key]
+        if key not in str1_tokens:
+            union.extend([key] * count_2)
+            
+    if not union:
+        return 65536
+    
+    return int((len(intersec) / len(union)) * 65536)
