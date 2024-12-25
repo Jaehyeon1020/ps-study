99cb181a688ce81de2b1076ff8e9a8c3001b7053
Jaehyeon Kim <kjhkk1020@g.skku.edu>
2024-12-25 22:57:07 +0900

    [level 2] Title: [1차] 캐시, Time: 34.73 ms, Memory: 128 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
@@ -1,56 +1,22 @@
 import java.util.*;
 
 class Solution {
-    class CacheElement {
-        public String city;
-        public int time;
-        
-        public CacheElement(String city, int time) {
-            this.city = city;
-            this.time = time;
-        }
-    };
-        
     public int solution(int cacheSize, String[] cities) {
         if (cacheSize == 0) return cities.length * 5;
         
         int answer = 0;
         
-        var cache = new ArrayList<CacheElement>();
-        for (var city: cities) {
-            city = city.toLowerCase();
-            for (var element: cache) element.time += 1; // 시간 증가
+        var cache = new ArrayList<String>();
+        for (int i = 0; i < cities.length; i++) {
+            var city = cities[i].toLowerCase();
             
-            var cityFound = false;
-            
-            // 캐시에 해당 도시가 있는지 확인
-            for (var element: cache) {
-                if (element.city.equals(city)) {
-                    answer += 1;
-                    element.time = 0;
-                    cityFound = true;
-                    break;
-                }
-            }
-            
-            // 캐시에 없으면 추가
-            if (!cityFound) {
-                if (cache.size() < cacheSize) {
-                    cache.add(new CacheElement(city, 0));
-                } else {
-                    int maxIdx = 0;
-                    int maxTime = 0;
-                    for (int i = 0; i < cache.size(); i++) {
-                        if (cache.get(i).time > maxTime) {
-                            maxTime = cache.get(i).time;
-                            maxIdx = i;
-                        }
-                    }
-                    
-                    cache.remove(maxIdx);
-                    cache.add(new CacheElement(city, 0));
-                }
-                
+            if (cache.contains(city)) {
+                cache.remove(city);
+                cache.add(city);
+                answer += 1;
+            } else {
+                if (cache.size() == cacheSize) cache.remove(0);
+                cache.add(city);
                 answer += 5;
             }
         }
