5bb7df77a95fb3e1c91cb0c49f1705f1aa078a48
Jaehyeon Kim <kjhkk1020@g.skku.edu>
2025-03-09 18:12:06 +0900

    [Gold V] Title: LCS, Time: 140 ms, Memory: 18500 KB -BaekjoonHub

---
+++ 백준/Gold/9251. LCS/LCS.java
@@ -0,0 +1,41 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static String s1, s2;
+
+    static int getLcsLen() {
+        int[][] dp = new int[s1.length() + 1][s2.length() + 1];
+
+        for (int i = 1; i <= s1.length(); i++) {
+            char c1 = s1.charAt(i - 1);
+            for (int j = 1; j <= s2.length(); j++) {
+                char c2 = s2.charAt(j - 1);
+
+                if (c1 == c2) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        return dp[s1.length()][s2.length()];
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        s1 = br.readLine();
+        s2 = br.readLine();
+
+        int l = getLcsLen();
+
+        bw.write(Integer.toString(l));
+        bw.flush();
+
+        bw.close();
+        br.close();
+    }
+}
